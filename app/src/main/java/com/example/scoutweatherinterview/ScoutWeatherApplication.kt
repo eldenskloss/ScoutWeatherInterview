9d90eca63c5645489e1716b76e4cde487db58c8b
eldenskloss <eldenskloss@gmail.com>
2025-05-10 16:06:08 -0500

    Added initial hilt config code

---
+++ app/src/main/java/com/example/scoutweatherinterview/ScoutWeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.example.scoutweatherinterview
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ScoutWeatherApplication : Application()
