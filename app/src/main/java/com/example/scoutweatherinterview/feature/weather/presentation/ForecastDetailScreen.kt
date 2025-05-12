565cba97c718291f9b2da9c3c823a6a0a86f211c
eldenskloss <eldenskloss@gmail.com>
2025-05-11 22:32:24 -0500

    Format

--- app/src/main/java/com/example/scoutweatherinterview/feature/weather/presentation/ForecastDetailScreen.kt
+++ app/src/main/java/com/example/scoutweatherinterview/feature/weather/presentation/ForecastDetailScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.scoutweatherinterview.ui.theme.ScoutWeatherInterviewTheme
 
 @Composable
 fun ForecastDetailScreen(forecastDate: String) {
@@ -18,10 +16,10 @@ fun ForecastDetailScreen(forecastDate: String) {
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun ForecastDetailScreenPreview() {
+// @Preview(showBackground = true)
+// @Composable
+// fun ForecastDetailScreenPreview() {
 //    ScoutWeatherInterviewTheme {
 //        ForecastDetailScreen()
 //    }
-//}
+// }
