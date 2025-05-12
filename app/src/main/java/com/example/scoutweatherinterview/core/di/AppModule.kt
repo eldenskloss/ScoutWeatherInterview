c058ce0ababb3ec9bb1b2444608e87643b7cf533
eldenskloss <eldenskloss@gmail.com>
2025-05-12 13:44:23 -0500

    Added in memory caching

--- app/src/main/java/com/example/scoutweatherinterview/core/di/AppModule.kt
+++ app/src/main/java/com/example/scoutweatherinterview/core/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.example.scoutweatherinterview.core.di
 
 import android.app.Application
 import com.example.scoutweatherinterview.core.Constants.WEATHER_API_BASE_URL
+import com.example.scoutweatherinterview.core.network.CacheInterceptor
 import com.example.scoutweatherinterview.feature.weather.data.repository.FetchForecastApi
 import com.example.scoutweatherinterview.feature.weather.data.repository.FetchForecastRepositoryImpl
 import com.example.scoutweatherinterview.feature.weather.data.repository.LocationRepositoryImpl
@@ -11,18 +12,33 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     @Provides
     @Singleton
-    fun providesFetchForecastApi(): FetchForecastApi {
+    fun provideOkHttpClient(application: Application): OkHttpClient {
+        val tenMegabytes = 10L * 1024L * 1024L
+        return OkHttpClient().newBuilder()
+            .cache(Cache(File(application.cacheDir, "http-cache"), tenMegabytes))
+            .addNetworkInterceptor(CacheInterceptor())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesFetchForecastApi(httpClient: OkHttpClient): FetchForecastApi {
         return Retrofit
             .Builder()
+            .client(httpClient)
             .baseUrl(WEATHER_API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
