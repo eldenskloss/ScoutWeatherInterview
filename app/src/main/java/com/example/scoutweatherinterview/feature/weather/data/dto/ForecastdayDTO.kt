aee255d42d795d58ce050563350c00e45efa676b
eldenskloss <eldenskloss@gmail.com>
2025-05-14 13:11:52 -0500

    Deleted unused DTO variables + Created simple unit test to test converter

--- app/src/main/java/com/example/scoutweatherinterview/feature/weather/data/dto/ForecastdayDTO.kt
+++ app/src/main/java/com/example/scoutweatherinterview/feature/weather/data/dto/ForecastdayDTO.kt
@@ -1,9 +1,7 @@
 package com.example.scoutweatherinterview.feature.weather.data.dto
 
 data class ForecastdayDTO(
-    val astro: AstroDTO,
     val date: String,
     val date_epoch: Int,
-    val day: DayDTO,
-    val hour: List<HourDTO>
+    val day: DayDTO
 )
--- app/src/test/java/com/example/scoutweatherinterview/ExampleUnitTest.kt
+++ app/src/test/java/com/example/scoutweatherinterview/ExampleUnitTest.kt
@@ -1,5 +1,13 @@
 package com.example.scoutweatherinterview
 
+import com.example.scoutweatherinterview.feature.weather.data.dto.ConditionDTO
+import com.example.scoutweatherinterview.feature.weather.data.dto.CurrentDTO
+import com.example.scoutweatherinterview.feature.weather.data.dto.DayDTO
+import com.example.scoutweatherinterview.feature.weather.data.dto.ForecastDTO
+import com.example.scoutweatherinterview.feature.weather.data.dto.ForecastResponseDTO
+import com.example.scoutweatherinterview.feature.weather.data.dto.ForecastdayDTO
+import com.example.scoutweatherinterview.feature.weather.data.dto.LocationDTO
+import com.example.scoutweatherinterview.feature.weather.data.dto.mapper.toForecast
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -9,8 +17,46 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun test_dto_converter() {
+        val dto = ForecastResponseDTO(
+            current = CurrentDTO(10, ConditionDTO(10, "Cloudy", "")),
+            forecast = ForecastDTO(
+                listOf(
+                    // 2023-10-26 was a Thursday
+                    ForecastdayDTO(
+                        "2023-10-26",
+                        100,
+                        DayDTO(
+                            1,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0,
+                            ConditionDTO(10, "Cloudy", ""),
+                            1,
+                            1,
+                            1,
+                            1,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0,
+                            1.0
+                        )
+                    )
+                )
+            ),
+            location = LocationDTO("London", 10.0, "", 1000, 10.0, "", "", "")
+        )
+        val convertedDTO = dto.toForecast()
+        assertEquals(dto.forecast.forecastday.first().date, convertedDTO.forecasts.first().date)
+        assertEquals("Thursday", convertedDTO.forecasts.first().day)
     }
 }
